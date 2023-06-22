1165ea6b98284324a77e63051485009981fb1668
obiwac <obiwac@gmail.com>
2023-06-22 14:20:27 +0200

    batman: Get distributed ARP table to build

--- sys/compat/linuxkpi/common/include/asm/unaligned.h
+++ sys/compat/linuxkpi/common/include/asm/unaligned.h
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2020,2023 The FreeBSD Foundation
+ * Copyright (c) 2023 Aymeric Wibo <obiwac@freebsd.org>
  *
  * This software was developed by Björn Zeeb under sponsorship from
  * the FreeBSD Foundation.
@@ -95,5 +96,14 @@ get_unaligned_be64(const void *p)
 
 	return (be64_to_cpup((const __be64 *)p));
 }
+
+#define	get_unaligned(ptr)	({		\
+	struct {				\
+		typeof(*(ptr))	x;		\
+	} __attribute__((packed)) const *	\
+	nptr = __DECONST(void *, (ptr));	\
+						\
+	nptr->x;				\
+})
 
 #endif	/* _LINUXKPI_ASM_UNALIGNED_H */
