4a381e7438cd882500d505b7f9fabb1a7d4d4f89
obiwac <obiwac@gmail.com>
2023-06-22 13:32:11 +0200

    batman: Get bridge loop avoidance to build

---
+++ sys/compat/linuxkpi/common/include/net/arp.h
@@ -0,0 +1,28 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2023 Aymeric Wibo <obiwac@freebsd.org>
+ */
+
+#ifndef	_LINUXKPI_NET_ARP_H
+#define	_LINUXKPI_NET_ARP_H
+
+#include <linux/netdevice.h>
+#include <linux/skbuff.h>
+
+static inline struct sk_buff *
+arp_create(int type, int ptype, __be32 dest_ip, struct net_device *dev, __be32 src_ip, unsigned char const *dest_hw, unsigned char const *src_hw, unsigned char const *target_hw)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+	return (NULL);
+}
+
+static inline unsigned int
+arp_hdr_len(struct net_device const *dev)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+	return (0);
+}
+
+#endif	/* _LINUXKPI_NET_ARP_H */
