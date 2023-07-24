ae6d14bc1e874b9115b524091845c81971e11ea7
obiwac <obiwac@gmail.com>
2023-07-24 23:01:13 +0200

    linuxkpi/skbuff: Implement `skb_header_pointer`

--- sys/compat/linuxkpi/common/include/linux/skbuff.h
+++ sys/compat/linuxkpi/common/include/linux/skbuff.h
@@ -1177,7 +1177,11 @@ skb_header_pointer(struct sk_buff const *skb, int offset, int len, void *buffer)
 {
 
 	SKB_TODO();
-	return (NULL);
+
+	if (skb_headlen(__DECONST(struct sk_buff *, skb)) - offset >= len)
+		return ((char *)buffer + offset);
+
+	return (buffer);
 }
 
 static inline bool
