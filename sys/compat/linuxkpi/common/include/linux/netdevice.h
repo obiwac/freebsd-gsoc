486eadc6d7b964dc6f8a6d5c44953ab3b7915882
obiwac <obiwac@gmail.com>
2023-08-14 21:17:22 +0200

    linux/netdevice: Pass `M_NOWAIT` to `m_get3` so we don't have issues with non-sleepable locks

--- sys/compat/linuxkpi/common/include/linux/netdevice.h
+++ sys/compat/linuxkpi/common/include/linux/netdevice.h
@@ -629,7 +629,7 @@ netif_rx(struct sk_buff *skb)
 	/* Create mbuf from skbuff. */
 	/* TODO Should this be a M_EXT? What is M_PKTHDR for? */
 
-	m = m_get3(len, M_WAITOK, MT_DATA, M_PKTHDR);
+	m = m_get3(len, M_NOWAIT, MT_DATA, M_PKTHDR);
 	if (m == NULL)
 		return;
 	m->m_pkthdr.rcvif = ifp;
@@ -747,7 +747,7 @@ dev_queue_xmit(struct sk_buff *skb)
 	/* Create mbuf from skbuff. */
 	/* TODO Should this be a M_EXT? What is M_PKTHDR for? */
 
-	m = m_get3(len, M_WAITOK, MT_DATA, M_PKTHDR);
+	m = m_get3(len, M_NOWAIT, MT_DATA, M_PKTHDR);
 	if (m == NULL)
 		return (EIO);
 	m->m_pkthdr.len = len;
