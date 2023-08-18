65f40509b0d0a924a5469634212bb69c2b7a28a5
obiwac <obiwac@gmail.com>
2023-08-18 18:02:06 +0200

    linux/bitsperlong: Replace GPL version

--- sbin/ifconfig/ifconfig_netlink.c
+++ sbin/ifconfig/ifconfig_netlink.c
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
  * Copyright (c) 2022 Alexander V. Chernikov <melifaro@FreeBSD.org>
+ * Copyright (c) 2023 Aymeric Wibo <obiwac@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- sys/compat/linux/linux_netlink.c
+++ sys/compat/linux/linux_netlink.c
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2022 Alexander V. Chernikov
+ * Copyright (c) 2023 Aymeric Wibo <obiwac@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
