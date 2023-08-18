b1874f63e30830eddcf993ecca496c4014d2347f
obiwac <obiwac@gmail.com>
2023-08-18 18:24:09 +0200

    linux/threads: Replace GPL version

--- sys/compat/linuxkpi/common/include/linux/rculist.h
+++ sys/compat/linuxkpi/common/include/linux/rculist.h
@@ -1,6 +1,7 @@
 /*-
  * Copyright (c) 2015 François Tigeot
  * Copyright (c) 2016-2020 Mellanox Technologies, Ltd.
+ * Copyright (c) 2023 Aymeric Wibo <obiwac@freebsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
