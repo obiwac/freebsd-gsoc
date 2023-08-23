f491d2149d6f7f9363db740f190dc300168f079d
obiwac <obiwac@gmail.com>
2023-08-23 12:50:25 +0200

    linux/pr_debug: Clean up

--- sys/compat/linuxkpi/common/include/linux/pr_debug.h
+++ sys/compat/linuxkpi/common/include/linux/pr_debug.h
@@ -18,20 +18,20 @@
  * unless DEBUG is defined:
  */
 
-// #ifdef DEBUG
+#ifdef DEBUG
 extern int linuxkpi_debug;
 #define pr_debug(fmt, ...)					\
 	do {							\
-		/*if (linuxkpi_debug)*/				\
+		if (linuxkpi_debug)				\
 			printf(fmt, ##__VA_ARGS__);		\
 	} while (0)
 #define pr_devel(fmt, ...) \
 	log(LOG_DEBUG, pr_fmt(fmt), ##__VA_ARGS__)
-// #else
-// #define pr_debug(fmt, ...) \
-// 	({ if (0) log(LOG_DEBUG, fmt, ##__VA_ARGS__); 0; })
-// #define pr_devel(fmt, ...) \
-// 	({ if (0) log(LOG_DEBUG, pr_fmt(fmt), ##__VA_ARGS__); 0; })
-// #endif
+#else
+#define pr_debug(fmt, ...) \
+	({ if (0) log(LOG_DEBUG, fmt, ##__VA_ARGS__); 0; })
+#define pr_devel(fmt, ...) \
+	({ if (0) log(LOG_DEBUG, pr_fmt(fmt), ##__VA_ARGS__); 0; })
+#endif
 
 #endif	/* _LINUXKPI_LINUX_PR_DEBUG_H */
